39bd8d7d95e0f113f6d06f71dc4b33379a700a62
Ismael Morel <ismaelmorel84@gmail.com>
2021-09-15 16:02:34 -0300

    clase 1

---
+++ clase 1/app.js
@@ -0,0 +1,74 @@
+//COMENTARI ODE UNA SOLA LINEA
+/*
+COMENTARIO MULTILINEAL
+*/
+/*
+//DATOS PRIMITIVOS
+
+//STRING cadena de caracteres
+
+'HOLA' '50505' 'isma'
+
+' '
+
+//NUMBERS
+
+8 25 17 2021
+
+//flaot number
+
+1.3 2.43 
+
+//boolean
+true false
+
+
+/*
+3 formas de declarar variables, var let const
+*/ 
+
+//NO UTILIZAMOS MAS VAR
+
+//VAR Y LET MUTABLES
+
+/*
+
+var nombre = 'isma'
+
+var apellido = 'morel'
+
+var edad = 18
+
+let ciudad = 'bsas'
+
+//inmutable const
+
+const pais = 'argentina'
+
+*/
+
+//Constructor -> nombre de la variable = valor
+/*
+
+let nombreProfesor = 'julian'
+
+const nombreCamada = 22405
+*/
+
+//operaciones basicas + - * /
+
+//sentencia prompt() seria como un input para q el usuaria ingrese un dato.
+
+//let mensaje = prompt('Ingrese un dato')
+
+
+//console.log() mostrar mensaje en consola
+
+console.log('hola mundo')
+console.log('primer console.log')
+
+//alert
+//muestra alerta de forma abrupta
+
+alert('cuidado tenes un error')
+
