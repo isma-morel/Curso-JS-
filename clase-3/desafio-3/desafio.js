bac08b8aea427b2b9d6df16587b7bb58af857ab8
Ismael Morel <ismaelmorel84@gmail.com>
2021-09-21 01:32:51 -0300

    agregando cambios

--- clase-3/desafio-3/desafio.js
+++ clase-3/desafio-3/desafio.js
@@ -1,22 +1,40 @@
-//algoritmo foco
+//algoritmo pileta
 
 let piletaDefault = 0;
-let options = confirm('Deseas cargar la pileta?')
+let options = confirm('Deseas cargar la pileta?');
+let tope = 0;
+
+function message() {
+    piletaDefault -= 10;
+    console.log(`Se cayo un poco de agua!. La pileta tiene ${piletaDefault} litros`);
+    alert(`Se cayo un poco de agua!. La pileta tiene ${piletaDefault} litros`);
+    tope ++;
+    if (tope >= 20) {
+        clearInterval(intervalo)
+    }
+}
+
+function intervalo() {
+    intervalo = setInterval(message,5000);
+}
+
+
 
 if (options === true) {
     console.log('La pileta esta vacia. Tienes que llenarla!')
     alert('La pileta esta vacia. Tienes que llenarla!')
-    for (let agua = Number(prompt('Cuantos litros de agua deseas agregar a la pileta?'));piletaDefault < 1600; piletaDefault = agua + piletaDefault) {
+    for (let agua = Number(prompt('Cuantos litros/seg de agua deseas agregar?'));piletaDefault < 1600; piletaDefault = agua + piletaDefault) {
         console.log(`Estas cargando la pileta actualmente tiene ${piletaDefault} litros`)
         alert(`Estas cargando la pileta actualmente tiene ${piletaDefault} litros`)
     }
     if (piletaDefault === 1600) {
-        console.log(`Has llenado la pileta. Actualmente tiene ${piletaDefault} litros`)
-        alert(`Has llenado la pileta. Actualmente tiene ${piletaDefault} litros`)
+        console.log(`Has llenado la pileta. Actualmente tiene ${piletaDefault} litros`);
+        alert(`Has llenado la pileta. Actualmente tiene ${piletaDefault} litros`);
     } else {
-        console.log(`Has sobrepasado la maxima capacidad de la pileta. Actualmente tiene ${piletaDefault} litros`)
-        alert(`Has sobrepasado la maxima capacidad de la pileta. Actualmente tiene ${piletaDefault} litros`)
+        console.log(`Has sobrepasado la maxima capacidad de la pileta. Actualmente tiene ${piletaDefault} litros`);
+        alert(`Has sobrepasado la maxima capacidad de la pileta. Actualmente tiene ${piletaDefault} litros`);
     }
+    intervalo()
 } else {
     console.log('Vuelva pronto!')
     alert('Vuelva pronto!')
